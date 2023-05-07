bd859d1fc9241d659b2414beb97c1f6a6ed4f172
Daniel Pifer <daniel.pifer13@gmail.com>
2023-05-06 19:29:14 -0500

    login activity added

---
+++ app/src/main/java/com/example/serialbowl/LoginActivity.java
@@ -0,0 +1,44 @@
+package com.example.serialbowl;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void Login(View view){
+        //todo
+        EditText emailET = (EditText) findViewById(R.id.userNameEditText);
+        EditText passwordET = (EditText) findViewById(R.id.passwordEditText);
+
+        String email = emailET.getText().toString();
+        String password = passwordET.getText().toString();
+
+        if (email.equals("admin") && password.equals("password")){
+            continueToMain();
+            Toast.makeText(LoginActivity.this, "Logged in!", Toast.LENGTH_LONG).show();
+        }
+
+        else {
+            Toast.makeText(LoginActivity.this, "Invalid Login!", Toast.LENGTH_LONG).show();
+        }
+
+
+    }
+
+    private void continueToMain(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
--- app/src/main/java/com/example/serialbowl/SettingsActivity.java
+++ app/src/main/java/com/example/serialbowl/SettingsActivity.java
@@ -2,7 +2,10 @@ package com.example.serialbowl;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class SettingsActivity extends MainActivity {
 
@@ -10,5 +13,22 @@ public class SettingsActivity extends MainActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+    }
+
+    public void saveAPIChanges(View view){
+        //todo
+        returnHome();
+    }
+
+    public void Logout(View view){
+        Toast.makeText(SettingsActivity.this, "Logged out!", Toast.LENGTH_LONG).show();
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+
+    private void returnHome(){
+        Toast.makeText(SettingsActivity.this, "Settings saved!", Toast.LENGTH_LONG).show();
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
